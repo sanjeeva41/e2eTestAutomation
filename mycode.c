a54e37dd59a8c8be2ff244ff0b3ea05d2cfd4875
sanjeeva41 <72072315+sanjeeva41@users.noreply.github.com>
2026-03-11 11:56:38 +0100

    Update mycode.c
    fgdft4354

--- mycode.c
+++ mycode.c
@@ -1,3 +1,7 @@
-int subtraction(int a, int b) {
+/*
+* REQ-ID-1: The function shall take integer variables as arguments and perform the addition of the two input parameters and return the result.
+*/
+
+int addition(int a, int b) {
   return (a - b);//just
 }
